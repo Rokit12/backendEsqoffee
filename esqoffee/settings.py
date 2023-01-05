ce43f46b5149a7c44d0be06ab2f2c053ec757688
Jan Ndungu <ndungujan23@protonmail.com>
2023-01-05 23:01:35 +0300

    added allowed host url

--- esqoffee/settings.py
+++ esqoffee/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOST = ['*']
+ALLOWED_HOST = ['web-production-7aa3.up.railway.app']
 
 # Application definition
 
