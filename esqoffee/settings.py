79e61480ae9942175d128fd1e00f1a1310ecfcac
Jan Ndungu <ndungujan23@protonmail.com>
2023-01-06 23:24:05 +0300

    implemented blog app for starters

--- esqoffee/settings.py
+++ esqoffee/settings.py
@@ -34,9 +34,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # rest framework
+    'rest_framework',
+    'rest_framework.authtoken',
+    # documentation
+    'drf_yasg',
+    # apps
+    'blog',
     'shop',
-    'contact',
 ]
 
 MIDDLEWARE = [
@@ -136,4 +141,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
--- esqoffee/urls.py
+++ esqoffee/urls.py
@@ -3,12 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from contact import views as contact_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-    path('contact', contact_views.contact, name='contact'),
 ]
 
 if settings.DEBUG:
