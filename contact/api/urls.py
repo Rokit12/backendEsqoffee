5a0b01256659c2e771d2a736018c36d041b298c1
Jan Ndungu <ndungujan23@protonmail.com>
2023-01-08 19:44:47 +0300

    removed rest framework stuff

--- contact/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import (
-    CreateContactAPIView
-)
-
-urlpatterns = [
-    path('', CreateContactAPIView.as_view(), name="contact"),
-]
--- esqoffee/settings.py
+++ esqoffee/settings.py
@@ -35,10 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework',
-    'rest_framework.authtoken',
-    'drf_yasg',
-
     'blog',
     'contact',
     'shop',
@@ -49,21 +45,6 @@ INSTALLED_APPS = [
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
-
-REST_FRAMEWORK = {
-    "DEFAULT_RENDERER_CLASSES": (
-        "rest_framework.renderers.JSONRenderer",
-        "rest_framework.renderers.BrowsableAPIRenderer",
-    ),
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
-    ),
-    "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
-    ),
-}
 
 # SWAGGER_SETTINGS = {
 #     'VALIDATOR_URL': 'http://localhost:8000',
