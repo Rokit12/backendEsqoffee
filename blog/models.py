8e5a44d4be6521dec02b6af783291a6acc32d7b1
Jan Ndungu <ndungujan23@protonmail.com>
2023-01-08 23:12:24 +0300

    implemented post list page

--- blog/models.py
+++ blog/models.py
@@ -29,6 +29,7 @@ class Post(models.Model):
         ('published', 'Published'),
     )
     title = models.CharField(max_length=250)
+    poster = models.ImageField(upload_to='blog', blank=True)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
@@ -52,10 +53,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail',
-                       args=[self.publish.year,
-                             self.publish.month,
-                             self.publish.day, self.slug])
+        return reverse('blog:post_detail', args=[self.id])
 
 
 class Comment(models.Model):
--- blog/views.py
+++ blog/views.py
@@ -44,6 +44,7 @@ def post_list(request, tag_slug=None):
         'posts': posts,
         'tag': tag,
     }
+    print(posts.end_index())
     return render(request, template, context)
 
 
