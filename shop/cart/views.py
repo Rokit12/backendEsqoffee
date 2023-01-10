dc2904d8dc6be9d6e25868cca326abb3baa03112
Jan Ndungu <ndungujan23@protonmail.com>
2023-01-10 14:57:35 +0300

    add to cart now functioning

--- shop/cart/views.py
+++ shop/cart/views.py
@@ -9,12 +9,15 @@ from .forms import CartAddProductForm
 def cart_add(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
+
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(product=product,
-                 quantity=cd['quantity'],
+                 quantity=1,
                  override_quantity=cd['override'])
+    else:
+        cart.add(product=product, quantity=1, override_quantity=False)
     return redirect('cart:cart_detail')
 
 
